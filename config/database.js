ebde572c3264cd60c068f87c36423e5aa084d7d1
Leslie Saliba <lesliesaliba@gmail.com>
2023-10-19 13:40:23 +0200

    created .env.example

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
-  password:'',
+  password: process.env.PASSWORD,
   database: process.env.DB_NAME
 });
 
